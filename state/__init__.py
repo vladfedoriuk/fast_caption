5128dab5eae5fb264d4739a45740cceb5186bfd2
vladfedoriuk <vlad.fedoriuk2000@gmail.com>
2022-01-16 22:26:32 +0100

    Added pytest and testing database

--- state/__init__.py
+++ state/__init__.py
@@ -8,6 +8,7 @@ from sqlalchemy.orm import sessionmaker
 
 settings = get_settings()
 
+print('***********', settings.postgres_url)
 engine = create_async_engine(settings.postgres_url, echo=True, future=True)
 
 
--- state/versions/env.py
+++ state/versions/env.py
@@ -16,6 +16,8 @@ settings = get_settings()
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", settings.postgres_url)
+
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
